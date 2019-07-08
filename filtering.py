4a729366d7c31536a2f3852c70f53f5f17252ca8
Michel Lujano <lvm3632@gmail.com>
2019-07-07 22:02:35 -0500

    last commit

--- filtering.py
+++ filtering.py
@@ -9,13 +9,11 @@ for lst in yourResult:
     for j, item in enumerate(lst):
         lst[j] = item.replace('-', '').replace(']', '')
 
-
 for line in range(len(yourResult)):
-    print(yourResult[line][1])
+     yourResult[line][1] = yourResult[line][1].split('/', 1)[0].replace(".","")
+
 
 with open("out.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(yourResult)
-print("hi234")
-
 
