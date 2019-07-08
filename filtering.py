baf4b6eaff3a3963ffdeab049bf8b6da208ff916
Michel Lujano <lvm3632@gmail.com>
2019-07-07 22:45:21 -0500

    Last commit

--- filtering.py
+++ filtering.py
@@ -1,7 +1,7 @@
 import re
 import csv
 
-myFile = open('historietas.txt', 'r')
+myFile = open('prueba.txt', 'r')
 
 yourResult = [line.split('[') for line in myFile.readlines()]
 
@@ -13,7 +13,7 @@ for line in range(len(yourResult)):
      yourResult[line][1] = yourResult[line][1].split('/', 1)[0].replace(".","")
 
 
-with open("out.csv", "w", newline="") as f:
+with open("prueba.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(yourResult)
 
