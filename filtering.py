d4b2a0defbeb9b5788dcb5e41fbf3420bc89b07d
Michel Lujano <lvm3632@gmail.com>
2019-07-07 20:50:49 -0500

    Add existing file

---
+++ filtering.py
@@ -0,0 +1,18 @@
+import re
+import csv
+
+myFile = open('historietas.txt', 'r')
+
+yourResult = [line.split('[') for line in myFile.readlines()]
+
+for lst in yourResult:
+    for j, item in enumerate(lst):
+        lst[j] = item.replace('-', '').replace(']', '')
+
+
+for line in range(len(yourResult)):
+    print(yourResult[line][1])
+
+with open("out.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(yourResult)
